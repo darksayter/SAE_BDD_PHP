1ec4047fd37d538a5d34d0b7f35c89444f122b5e
DUVET Alexandre <alexandre.duvet@etu.univ-littoral.fr>
2023-12-05 14:48:10 +0100

    Update tables.sql

--- script/creation table/tables.sql
+++ script/creation table/tables.sql
@@ -176,64 +176,54 @@ CREATE TABLE Profil (
 
 -- Table for AnimeFav --
 CREATE TABLE AnimesFav(
-    id_animefav INT PRIMARY KEY,
     id_profil INT REFERENCES Profil(id_profil),
     id_anime INT REFERENCES Anime(id_anime)
 );
 
 -- Table for MangaFav --
 CREATE TABLE MangasFav(
-    id_mangafav INT PRIMARY KEY,
     id_profil INT REFERENCES Profil(id_profil),
     id_manga INT REFERENCES Manga(id_manga)
 );
 
 -- Table for PersonnageFav --
 CREATE TABLE PersonnagesFav(
-    id_personnagefav INT PRIMARY KEY,
     id_profil INT REFERENCES Profil(id_profil),
     id_pers INT REFERENCES Personnage(id_pers)
 );
 
 -- Table for VoiceActorFav --
 CREATE TABLE VoiceActorsFav(
-    id_voiceactorfav INT PRIMARY KEY,
     id_profil INT REFERENCES Profil(id_profil),
     id_voiceactor INT REFERENCES VoiceActor(id_voiceactor)
 );
 
 CREATE TABLE GenresFav(
-    id_genrefav INT PRIMARY KEY,
     id_profil INT REFERENCES Profil(id_profil),
     id_genre INT REFERENCES Genre(id_genre)
 );
 
 CREATE TABLE ThemesFav(
-    id_themefav INT PRIMARY KEY,
     id_profil INT REFERENCES Profil(id_profil),
     id_theme INT REFERENCES Theme(id_theme)
 );
 
 CREATE TABLE AnimeGenre(
-    id_animegenre INT PRIMARY KEY,
     id_anime INT REFERENCES Anime(id_anime),
     id_genre INT REFERENCES Genre(id_genre)
 );
 
 CREATE TABLE AnimeTheme(
-    id_animetheme INT PRIMARY KEY,
     id_anime INT REFERENCES Anime(id_anime),
     id_theme INT REFERENCES Theme(id_theme)
 );
 
 CREATE TABLE MangaGenre(
-    id_magengenre INT PRIMARY KEY,
     id_manga INT REFERENCES Manga(id_manga),
     id_genre INT REFERENCES Genre(id_genre)
 );
 
 CREATE TABLE MangaTheme(
-    id_magengenre INT PRIMARY KEY,
     id_manga INT REFERENCES Manga(id_manga),
     id_theme INT REFERENCES Theme(id_theme)
 );
