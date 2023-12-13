0ccf76ad02b8fcb3ae17b04cf16c5fe2f5300b7b
scipionc <clementduchemin6@gmail.com>
2023-12-13 11:21:58 +0100

    a

--- script/fonctions/votesaffrontement.sql
+++ script/fonctions/votesaffrontement.sql
@@ -11,7 +11,12 @@ BEGIN
     SET vote_anime1 = vote_1,
         vote_anime2 = vote_2
     WHERE id_tournoianime = id_tournoi
-      AND ((id_anime1 = id_1 AND id_anime2 = id_2) OR (id_anime1 = id_2 AND id_anime2 = id_1));
+      AND ((id_anime1 = id_1 AND id_anime2 = id_2));
+    UPDATE AffrontementAnime
+    SET vote_anime1 = vote_2,
+        vote_anime2 = vote_1
+    WHERE id_tournoianime = id_tournoi
+      AND ((id_anime1 = id_2 AND id_anime2 = id_1));
 END;
 $$ LANGUAGE plpgsql;
 
@@ -29,7 +34,12 @@ BEGIN
     SET vote_manga1 = vote_1,
         vote_manga2 = vote_2
     WHERE id_tournoimanga = id_tournoi
-      AND ((id_manga1 = id_1 AND id_manga2 = id_2) OR (id_manga1 = id_2 AND id_manga2 = id_1));
+      AND ((id_manga1 = id_1 AND id_manga2 = id_2));
+    UPDATE AffrontementManga
+    SET vote_manga1 = vote_2,
+        vote_manga2 = vote_1
+    WHERE id_tournoimanga = id_tournoi
+      AND ((id_manga1 = id_2 AND id_manga2 = id_1));
 END;
 $$ LANGUAGE plpgsql;
 
@@ -47,7 +57,12 @@ BEGIN
     SET vote_personnage1 = vote_1,
         vote_personnage2 = vote_2
     WHERE id_tournoipersonnage = id_tournoi
-      AND ((id_personnage1 = id_1 AND id_personnage2 = id_2) OR (id_personnage1 = id_2 AND id_personnage2 = id_1));
+      AND ((id_personnage1 = id_1 AND id_personnage2 = id_2));
+    UPDATE AffrontementPersonnage
+    SET vote_personnage1 = vote_2,
+        vote_personnage2 = vote_1
+    WHERE id_tournoipersonnage = id_tournoi
+      AND ((id_personnage1 = id_2 AND id_personnage2 = id_1));
 END;
 $$ LANGUAGE plpgsql;
 
