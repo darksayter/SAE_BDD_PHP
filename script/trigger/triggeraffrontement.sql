842120771366e500a0b01ef2d79bec59b0119c21
scipionc <clementduchemin6@gmail.com>
2023-12-13 11:15:27 +0100

    w

--- script/trigger/triggeraffrontement.sql
+++ script/trigger/triggeraffrontement.sql
@@ -41,3 +41,95 @@ CREATE TRIGGER MiseAJourClassementAnime
 AFTER UPDATE OF vote_anime1, vote_anime2 ON AffrontementAnime
 FOR EACH ROW
 EXECUTE FUNCTION MettreAJourClassementAnime();
+
+
+
+
+
+
+CREATE OR REPLACE FUNCTION MettreAJourClassementPersonnage()
+RETURNS TRIGGER AS $$
+DECLARE
+    participants INT[];
+    total_participants INT;
+    new_etapes_winner INT;
+    new_etapes_loser INT;
+    new_place_winner FLOAT;
+    new_place_loser FLOAT;
+BEGIN
+    IF NEW.vote_personnage1 > 0 OR NEW.vote_personnage2 > 0 THEN
+        -- Mettre à jour l'étape du perdant et du gagnant
+        new_etapes_winner := NEW.etapes + 1;
+        new_etapes_loser := NEW.etapes;
+
+        UPDATE ClassementPersonnage
+        SET etapes = CASE WHEN id_personnage = NEW.id_personnage1 THEN new_etapes_winner ELSE new_etapes_loser END
+        WHERE id_personnage IN (NEW.id_personnage1, NEW.id_personnage2) AND id_tournoipersonnage = NEW.id_tournoipersonnage;
+
+        -- Calcul du classement du participant
+        SELECT COUNT(*) * 2 INTO total_participants
+        FROM AffrontementPersonnage
+        WHERE etapes = 0 AND id_tournoipersonnage = NEW.id_tournoipersonnage;
+
+        new_place_winner := total_participants / (2 ^ new_etapes_winner);
+        new_place_loser := total_participants / (2 ^ new_etapes_loser);
+
+        UPDATE ClassementPersonnage
+        SET place = CASE WHEN id_personnage = NEW.id_personnage1 THEN new_place_winner ELSE new_place_loser END
+        WHERE id_personnage IN (NEW.id_personnage1, NEW.id_personnage2) AND id_tournoipersonnage = NEW.id_tournoipersonnage;
+
+    END IF;
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER MiseAJourClassementPersonnage
+AFTER UPDATE OF vote_personnage1, vote_personnage2 ON AffrontementPersonnage
+FOR EACH ROW
+EXECUTE FUNCTION MettreAJourClassementPersonnage();
+
+
+
+
+CREATE OR REPLACE FUNCTION MettreAJourClassementManga()
+RETURNS TRIGGER AS $$
+DECLARE
+    participants INT[];
+    total_participants INT;
+    new_etapes_winner INT;
+    new_etapes_loser INT;
+    new_place_winner FLOAT;
+    new_place_loser FLOAT;
+BEGIN
+    IF NEW.vote_manga1 > 0 OR NEW.vote_manga2 > 0 THEN
+        -- Mettre à jour l'étape du perdant et du gagnant
+        new_etapes_winner := NEW.etapes + 1;
+        new_etapes_loser := NEW.etapes;
+
+        UPDATE ClassementManga
+        SET etapes = CASE WHEN id_manga = NEW.id_manga1 THEN new_etapes_winner ELSE new_etapes_loser END
+        WHERE id_manga IN (NEW.id_manga1, NEW.id_manga2) AND id_tournoimanga = NEW.id_tournoimanga;
+
+        -- Calcul du classement du participant
+        SELECT COUNT(*) * 2 INTO total_participants
+        FROM AffrontementManga
+        WHERE etapes = 0 AND id_tournoimanga = NEW.id_tournoimanga;
+
+        new_place_winner := total_participants / (2 ^ new_etapes_winner);
+        new_place_loser := total_participants / (2 ^ new_etapes_loser);
+
+        UPDATE ClassementManga
+        SET place = CASE WHEN id_manga = NEW.id_manga1 THEN new_place_winner ELSE new_place_loser END
+        WHERE id_manga IN (NEW.id_manga1, NEW.id_manga2) AND id_tournoimanga = NEW.id_tournoimanga;
+
+    END IF;
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER MiseAJourClassementManga
+AFTER UPDATE OF vote_manga1, vote_manga2 ON AffrontementManga
+FOR EACH ROW
+EXECUTE FUNCTION MettreAJourClassementManga();
---
+++ script/trigger/triggercreationaffrontement.sql
@@ -0,0 +1,107 @@
+CREATE OR REPLACE FUNCTION CreerAffrontementAnime()
+RETURNS TRIGGER AS $$
+DECLARE
+    id_anime2 INT;
+BEGIN
+    -- Recherche d'un autre participant du même tournoi et de la même étape sans affrontement et qui n'est pas lui-même
+    SELECT ca.id_anime INTO id_anime2
+    FROM ClassementAnime ca
+    LEFT JOIN AffrontementAnime aa1 ON ca.id_anime = aa1.id_anime1 AND aa1.etapes = NEW.etapes
+    LEFT JOIN AffrontementAnime aa2 ON ca.id_anime = aa2.id_anime2 AND aa2.etapes = NEW.etapes
+    WHERE ca.id_tournoianime = NEW.id_tournoianime 
+    AND (aa1.id_anime1 IS NULL AND aa2.id_anime2 IS NULL) -- Vérifie si le participant n'a pas d'affrontement
+    AND ca.etapes = NEW.etapes AND ca.id_anime != NEW.id_anime
+    LIMIT 1;
+
+    -- Création de l'affrontement si un autre participant est trouvé
+    IF id_anime2 IS NOT NULL THEN
+        INSERT INTO AffrontementAnime (
+            id_anime1, id_anime2, id_tournoianime, vote_anime1, vote_anime2, etapes
+        )
+        VALUES (
+            NEW.id_anime, id_anime2,
+            NEW.id_tournoianime, 0, 0, NEW.etapes
+        );
+    END IF;
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER VerifierEtCreerAffrontementAnime
+AFTER UPDATE OF etapes ON ClassementAnime
+FOR EACH ROW
+EXECUTE FUNCTION CreerAffrontementAnime();
+
+
+CREATE OR REPLACE FUNCTION CreerAffrontementPersonnage()
+RETURNS TRIGGER AS $$
+DECLARE
+    id_personnage2 INT;
+BEGIN
+    -- Recherche d'un autre participant du même tournoi et de la même étape sans affrontement et qui n'est pas lui-même
+    SELECT cp.id_personnage INTO id_personnage2
+    FROM ClassementPersonnage cp
+    LEFT JOIN AffrontementPersonnage ap1 ON cp.id_personnage = ap1.id_personnage1 AND ap1.etapes = NEW.etapes
+    LEFT JOIN AffrontementPersonnage ap2 ON cp.id_personnage = ap2.id_personnage2 AND ap2.etapes = NEW.etapes
+    WHERE cp.id_tournoipersonnage = NEW.id_tournoipersonnage 
+    AND (ap1.id_personnage1 IS NULL AND ap2.id_personnage2 IS NULL) -- Vérifie si le participant n'a pas d'affrontement
+    AND cp.etapes = NEW.etapes AND cp.id_personnage != NEW.id_personnage
+    LIMIT 1;
+
+    -- Création de l'affrontement si un autre participant est trouvé
+    IF id_personnage2 IS NOT NULL THEN
+        INSERT INTO AffrontementPersonnage (
+            id_personnage1, id_personnage2, id_tournoipersonnage, vote_personnage1, vote_personnage2, etapes
+        )
+        VALUES (
+            NEW.id_personnage, id_personnage2,
+            NEW.id_tournoipersonnage, 0, 0, NEW.etapes
+        );
+    END IF;
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER VerifierEtCreerAffrontementPersonnage
+AFTER UPDATE OF etapes ON ClassementPersonnage
+FOR EACH ROW
+EXECUTE FUNCTION CreerAffrontementPersonnage();
+
+
+
+CREATE OR REPLACE FUNCTION CreerAffrontementManga()
+RETURNS TRIGGER AS $$
+DECLARE
+    id_manga2 INT;
+BEGIN
+    -- Recherche d'un autre participant du même tournoi et de la même étape sans affrontement et qui n'est pas lui-même
+    SELECT cm.id_manga INTO id_manga2
+    FROM ClassementManga cm
+    LEFT JOIN AffrontementManga am1 ON cm.id_manga = am1.id_manga1 AND am1.etapes = NEW.etapes
+    LEFT JOIN AffrontementManga am2 ON cm.id_manga = am2.id_manga2 AND am2.etapes = NEW.etapes
+    WHERE cm.id_tournoimanga = NEW.id_tournoimanga 
+    AND (am1.id_manga1 IS NULL AND am2.id_manga2 IS NULL) -- Vérifie si le participant n'a pas d'affrontement
+    AND cm.etapes = NEW.etapes AND cm.id_manga != NEW.id_manga
+    LIMIT 1;
+
+    -- Création de l'affrontement si un autre participant est trouvé
+    IF id_manga2 IS NOT NULL THEN
+        INSERT INTO AffrontementManga (
+            id_manga1, id_manga2, id_tournoimanga, vote_manga1, vote_manga2, etapes
+        )
+        VALUES (
+            NEW.id_manga, id_manga2,
+            NEW.id_tournoimanga, 0, 0, NEW.etapes
+        );
+    END IF;
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER VerifierEtCreerAffrontementManga
+AFTER UPDATE OF etapes ON ClassementManga
+FOR EACH ROW
+EXECUTE FUNCTION CreerAffrontementManga();
